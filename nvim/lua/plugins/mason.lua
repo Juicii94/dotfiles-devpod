a57ce9063f0ac839087f88a8c95c05b9bdf60817
evm <evm@pxmsoft.com>
2026-05-20 10:49:46 +0200

    changes

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "cds-lsp",
+        "css-lsp",
+        "html-lsp",
+        "jq",
+        "json-lsp",
+        "jsonlint",
+        "lua-language-server",
+        "markdown-toc",
+        "markdownlint-cli2",
+        "marksman",
+        "prettier",
+        "shfmt",
+        "sql-formatter",
+        "stylua",
+        "tree-sitter-cli",
+        "ts-standard",
+        "typescript-language-server",
+        "vtsls",
+        "xmlformatter",
+      },
+    },
+  },
+}
